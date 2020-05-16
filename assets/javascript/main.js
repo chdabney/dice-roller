ff1e11d90b7fb882cb172d2b7232f7bea414ed17
Chad <metacree@pop-os.localdomain>
2020-05-16 00:30:53 -0400

    added styling nd refactored inputs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,33 +1,49 @@
 //DOM selectors
 const rollButtonElement = document.querySelector('#rollButton')
-let userInputBoxElement = document.querySelector('#userInputBox')
 const showResultsButtonElement = document.querySelector('#showResultsButton')
 const displayAllRollsListElement = document.querySelector('#displayAllRollsList')
 const dieRollTotalsElement = document.querySelector('#dieRollTotals')
+let userInputBoxElement = document.querySelector('#userInputBox')
 let numberofSidesInputBoxElement = document.querySelector('#numberofSidesInputBox')
+const resetAllButtonElement = document.querySelector('#resetAllValues')
 
-//dire roll array
+//dice roll array
 const dieRollsArray = []
+
 
 // take user input and roll dice value
 rollButtonElement.addEventListener('click', function () {
     let numberofDice = userInputBoxElement.value
     let numberofSides = numberofSidesInputBoxElement.value
     let index = 0
+
     while (index < numberofDice) {
         index++
         let dieRollValue = Math.floor(Math.random() * numberofSides) + 1
         dieRollsArray.push(dieRollValue)
     }
-    let rollSum = eval(dieRollsArray.join('+'))
-    dieRollTotalsElement.innerHTML = `Total: ${rollSum}`
-})
+    let sumAllRolls = eval(dieRollsArray.join('+'))
+    dieRollTotalsElement.innerHTML = `<h2>Total:</h2> <h1> ${sumAllRolls}</h1>`
+    userInputBoxElement.value = " "
+    numberofSidesInputBoxElement.value = " "
 
+})
 //Show individual die rolls
 showResultsButtonElement.addEventListener('click', function () {
     let index = 0
     while (index < dieRollsArray.length) {
-        displayAllRollsListElement.innerHTML += `<li> ${dieRollsArray[index]} </li>`
+        displayAllRollsListElement.innerHTML += `<li class="dice"> ${dieRollsArray[index]} </li>`
         index++
     }
+        showResultsButtonElement.style.display = "none"
+        resetAllButtonElement.style.display = "inline"
+})
+
+//reset all
+resetAllButtonElement.addEventListener('click', function(){
+    dieRollsArray.length = []
+    dieRollTotalsElement.innerHTML = "<h2>Total:</h2>"
+    displayAllRollsListElement.innerHTML = " "
+    showResultsButtonElement.style.display = "inline"
+    resetAllButtonElement.style.display = "none"
 })
